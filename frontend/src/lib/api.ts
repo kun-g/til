e2ab3f368b8fbb855105463525c4d305559de241
Kun <kun.g@qq.com>
2025-05-16 20:08:22 +0800

    更新API请求，添加Authorization头部；修复tils.astro中标签名称的显示；在[slug].astro中引入fetchAllTILs以支持静态路径生成，并修正TIL属性的访问方式。

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,9 +1,14 @@
 const API_URL = import.meta.env.PUBLIC_STRAPI_API;
 
+const headers = {
+  'Authorization': `Bearer ${import.meta.env.PUBLIC_STRAPI_API_TOKEN}`
+}
+
 export async function fetchAllTILs() {
-  const res = await fetch(`${API_URL}/api/tils?populate=*&filters[publishedAt][$notNull]=true`);
-  console.log(`${API_URL}/api/tilss?populate=*&filters[publishedAt][$notNull]=true`)
+  const res = await fetch(`${API_URL}/api/tils?populate=*&filters[publishedAt][$notNull]=true&sort=createdAt:desc`, { headers });
   const json = await res.json();
+  console.log(json.data[1])
+
   if (json.data) {
     return json.data;
   } else {
@@ -12,7 +17,7 @@ export async function fetchAllTILs() {
 }
 
 export async function fetchTILBySlug(slug: string) {
-  const res = await fetch(`${API_URL}/api/tils?filters[slug][$eq]=${slug}&populate=*`);
+  const res = await fetch(`${API_URL}/api/tils?filters[slug][$eq]=${slug}&populate=*`, { headers });
   const json = await res.json();
   return json.data[0];
 }
